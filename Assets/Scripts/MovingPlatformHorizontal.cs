a9670e0e0a4c193319ca437fad84ec258e0f0546
johnsto3 <johnsto3@uoregon.edu>
2016-04-24 14:32:13 -0700

    Mitchell's Level 1 stuff
    See Title, Prefabs and Scripts Added and preliminary first level

---
+++ Assets/Scripts/MovingPlatformHorizontal.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MovingPlatformHorizontal : MonoBehaviour {
+
+	private float startx;
+	private bool right;
+	public float distance;
+	public float speed;
+
+	void Start () {
+		startx = transform.position.x;
+		right = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		if (right == true) {
+			Vector3 toTranslate = new Vector3 (speed * Time.deltaTime, 0f, 0f);
+			transform.Translate (toTranslate);
+		}
+		//once we hit a certain distance turn around
+		if (transform.position.x >= startx + distance) {
+			right = false;
+		}
+		if (right == false) {
+			Vector3 toTranslate = new Vector3 (-speed * Time.deltaTime, 0f, 0f);
+			transform.Translate (toTranslate);
+		}
+		//once we hit a certain distance turn around
+		if (transform.position.x <= startx - distance) {
+			right = true;
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/MovingPlatformVertical.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MovingPlatformVertical : MonoBehaviour {
+
+	private float startv;
+	private bool up;
+	public float distance;
+	public float speed;
+
+	void Start () {
+		startv = transform.position.y;
+		up = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		if (up == true) {
+			Vector3 toTranslate = new Vector3 (0f, speed * Time.deltaTime, 0f);
+			transform.Translate (toTranslate);
+		}
+		//once we hit a certain distance turn around
+		if (transform.position.y >= startv + distance) {
+			up = false;
+		}
+		if (up == false) {
+			Vector3 toTranslate = new Vector3 (0f, -speed * Time.deltaTime, 0f);
+			transform.Translate (toTranslate);
+		}
+		//once we hit a certain distance turn around
+		if (transform.position.y <= startv - distance) {
+			up = true;
+		}
+
+
+	}
+}
