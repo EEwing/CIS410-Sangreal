6f5b56f8c499c0449d8d46ddee1f80fdc950a626
johnsto3 <johnsto3@uoregon.edu>
2016-04-26 09:26:11 -0700

    Mitch edit (Physics)
    Altered movingplatforms and player controls, small change to my level

--- Assets/Scripts/MovingPlatformVertical.cs
+++ Assets/Scripts/MovingPlatformVertical.cs
@@ -15,7 +15,7 @@ public class MovingPlatformVertical : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
 
 		if (up == true) {
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -16,21 +16,27 @@ public class PlayerManager : Entity {
         return Physics.Raycast(transform.position, -Vector3.up, GetComponent<Collider>().bounds.extents.y + 0.1f);
     }
 
+	void Update(){
+		if (IsGrounded ()) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				GetComponent<Rigidbody> ().AddForce (new Vector3 (0f, jumpForce, 0f));
+			}
+
+		}
+	}
+
 // Update is called once per frame
-void Update () {
+void FixedUpdate () {
         Vector3 toTranslate = new Vector3(Input.GetAxis("Horizontal")*speed*Time.deltaTime, 0f, 0f);
-        if(IsGrounded()) {
-            if(Input.GetKeyDown(KeyCode.Space)) {
-                GetComponent<Rigidbody>().AddForce(new Vector3(0f, jumpForce, 0f));
-            }
+        
             //GetComponent<Rigidbody>().transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f, 0f));
-        } else {
+        
             //Vector3 force = new Vector3(Input.GetAxis("Horizontal")*speed * airModifier * Time.deltaTime, 0, 0);
             //if(force.x < 0 && GetComponent<Rigidbody>().velocity.x > 0 || force.x > 0 && GetComponent<Rigidbody>().velocity.x < 0) {
             //    force.x *= restitutionScale;
             //}
             //GetComponent<Rigidbody>().AddForce(force);
-        }
+        
         GetComponent<Rigidbody>().transform.Translate(toTranslate);
 	}
 }
