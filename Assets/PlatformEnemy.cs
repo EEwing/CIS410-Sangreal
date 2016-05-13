c18a0d2ae31c58629e38e2426a5a973013505afa
tracehagan <trace_hagan@yahoo.com>
2016-05-13 06:10:36 -0700

    Shitty combat that works
    Enemy will track towards player, if collides with player, damages
    player. If enemy collides with “weapon” then it takes damage. Works for
    Alpha. Needs polish for beta.

---
+++ Assets/PlatformEnemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformEnemy : MonoBehaviour {
+
+	public GameObject player; 
+	public int moveSpeed;
+	public int health;
+	public int engageDistance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (player.transform.position, transform.position) < engageDistance) {
+			transform.LookAt (player.transform.position); 
+			transform.position += transform.forward * moveSpeed * Time.deltaTime;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//Debug.Log("Collided with "+other.gameObject.name);
+		if (other.gameObject.tag == "Weapon") {
+			health--;
+			if (health == 0) {
+				Destroy (gameObject);
+			}
+		} 
+	}
+}
