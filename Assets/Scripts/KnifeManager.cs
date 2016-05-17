5974fc375f5156b2d65920fe3ab47db88fd5d1ab
tracehagan <trace_hagan@yahoo.com>
2016-05-17 15:10:33 -0700

    Knives destroy themselves

--- Assets/Scripts/KnifeManager.cs
+++ Assets/Scripts/KnifeManager.cs
@@ -2,15 +2,22 @@
 using System.Collections;
 
 public class KnifeManager : MonoBehaviour {
-
+	private int i;
 	// Use this for initialization
 	void Start () {
-	
+		i = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void FixedUpdate(){
+		i++;
+		if (i > 500) {
+			Destroy (gameObject);
+		}
 	}
 
 	void OnCollisionEnter(Collision other) {
